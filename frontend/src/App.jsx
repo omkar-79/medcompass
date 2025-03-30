f99b399d8ad5211092c66671472a49a96521b38d
omkar-79 <omkarbalekundri79@gmail.com>
2025-03-30 00:11:37 -0400

    Added Script Report

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Dashboard from "./pages/Dashboard";
 import { Routes, Route } from "react-router-dom"
 import PatientForm from "./pages/PatientForm";
+import ScriptReport from "./pages/ScriptReport";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Dashboard />} />
         <Route path='/addPatient' element={<PatientForm />} />
-
+        <Route path='/report' element={<ScriptReport />} />
       </Routes>
     </div>
   );
